34c9ca933f0e135bff4a1718392c3d96b1b92c1f
eigengrau01 <bryantroncosob@gmail.com>
2026-01-18 01:57:01 -0300

    feat: cli updated

--- main.go
+++ main.go
@@ -22,13 +22,19 @@ import (
 func main() {
 	net := mlp.CreateNetwork(784, 200, 10, 0.1)
 
-	train := flag.Bool("train", false, "Train the Neural Network")
+	train := flag.String("train", "", "Train the Neural Network with a specific file")
+	predict := flag.Bool("predict", false, "Evaluate the Neural Network")
 	file := flag.String("file", "", "File name of 28 x 28 PNG file to evaluate")
 	flag.Parse()
 
-	if *train {
-		mnistTrain(&net)
+	if *train != "" {
+		mnistTrain(&net, *train)
 		mlp.Save(net)
+	}
+
+	if *predict {
+		mlp.Load(&net)
+		mnistPredict(&net)
 	}
 
 	if *file != "" {
@@ -38,13 +44,13 @@ func main() {
 	}
 }
 
-func mnistTrain(net *mlp.Network) {
+func mnistTrain(net *mlp.Network, path string) {
 	rand.Seed(time.Now().UTC().UnixNano())
 	t1 := time.Now()
-	bar := progressbar.Default(5)
+	bar := progressbar.Default(5, "training...")
 
 	for epochs := 0; epochs < 5; epochs++ {
-		testFile, _ := os.Open("mnist_dataset/mnist_train.csv")
+		testFile, _ := os.Open(path)
 		r := csv.NewReader(bufio.NewReader(testFile))
 		for {
 			record, err := r.Read()
@@ -71,9 +77,49 @@ func mnistTrain(net *mlp.Network) {
 		testFile.Close()
 	}
 	elapsed := time.Since(t1)
-	fmt.Printf("\nTime taken to train: %s\n", elapsed)
+	fmt.Printf("Time taken to train: %s\n", elapsed)
 }
 
+func mnistPredict(net *mlp.Network) {
+	t1 := time.Now()
+	checkFile, _ := os.Open("mnist_dataset/mnist_test.csv")
+	defer checkFile.Close()
+	bar := progressbar.DefaultBytes(-1, "reading file...")
+
+	score := 0
+	r := csv.NewReader(bufio.NewReader(checkFile))
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		inputs := make([]float64, net.Inputs)
+		for i := range inputs {
+			if i == 0 {
+				inputs[i] = 1.0
+			}
+			x, _ := strconv.ParseFloat(record[i], 64)
+			inputs[i] = (x / 255.0 * 0.999) + 0.001
+		}
+		outputs := net.Predict(inputs)
+		best := 0
+		highest := 0.0
+		for i := 0; i < net.Outputs; i++ {
+			if outputs.At(i, 0) > highest {
+				best = i
+				highest = outputs.At(i, 0)
+			}
+		}
+		target, _ := strconv.Atoi(record[0])
+		if best == target {
+			score++
+		}
+		bar.Add(1)
+	}
+	elapsed := time.Since(t1)
+	fmt.Printf("Time taken to check: %s\n", elapsed)
+	fmt.Printf("score:%d/10000\n", score)
+}
 
 func printImage(img image.Image) {
 	var buf bytes.Buffer
